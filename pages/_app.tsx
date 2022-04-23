4c5aeecdcb6a2807d2b5ecd83b9038c27b197829
Carlos Ferreira <carlosferreira@outlook.com>
2022-04-23 11:01:35 +0100

    Added default page title to the site

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,17 @@
-import '../styles/main.scss';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+
 import MainLayout from '@/layouts/main';
+
+import '../styles/main.scss';
 
 function MyApp({ Component, pageProps }: AppProps)
 {
   return (
     <MainLayout>
+      <Head>
+        <title>Carlos.Tips - Collection of Random Tech Thoughts</title>
+      </Head>
       <Component {...pageProps} />
     </MainLayout>
   );
